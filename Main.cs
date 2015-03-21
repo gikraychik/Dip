247cf3056c3180e9f3a4d278c8cfbbf4cb05950a
George Kraychik <gik06@yandex.ru>
2015-03-21 17:42:49 +0300

    Strat1 works. Results achieved.

--- Main.cs
+++ Main.cs
@@ -36,7 +36,10 @@ namespace Diploma
 			this.v = v;
 			this.volume = volume;
 		}
-		       
+		public string ToString()
+		{
+			return dt.ToString() + " " + v + " " + volume;
+		}
 		public DateTime dt;
 		public double v;
 		public int volume;
@@ -58,18 +61,22 @@ namespace Diploma
 			his = new List<ArrayList>();
 			his.Add(this.state);
 		}
-		public Robot(double money, List<Data> d0, int last_data)
+		public Robot(double money, List<Data> d0, int last_data, List<double> st_params, double comm, int asset_type)
 		{
 			his = new List<ArrayList>();
 			d = new List<Data>();
+			this.strat_params = new List<double>();
 			this.pos = 0;
 			this.money = money;
 			this.dcn = (x => 0);
 			if (d0 != null)
 				this.d = d0;
-			this.dcn = this.TestStrat;
+			this.dcn = this.Strat1;
 			this.last_data = last_data;
 			this.l_price = 0.0;
+			this.pars = st_params;
+			this.commission_rate = comm;
+			this.asset_type = asset_type;
 			his.Add(this.state);
 		}
 		public int Trade(int amount, double price, bool is_last_operation)
@@ -77,21 +84,31 @@ namespace Diploma
 			int a = 0;
 			if (is_last_operation)
 			{
-				//Console.WriteLine("!!!!!!!!!!!!!!!!!!");
-				//Console.ReadKey();
 				if (pos != 0)		// открыта позиция, нужно ее закрыть
 				{
 					a = -pos;
 				}
 				else { a = 0; }
 			}
-			else { a = (int)(money < amount * price ? money / price : amount); }
+			else { a = (int)(money < amount * price ? CalcContracts(money, price) : amount); }
 			if (a == 0) return 0;
 			pos += a;
 			money -= a * price;
+			money -= Math.Abs(CalcCommission(a, price));
 			his.Add(this.state);
 			if (a > 0) last_price = price;
 			return a;
+		}
+		private int CalcContracts(double m, double p)
+		{
+			int n = (int)(m / p);
+			double c = CalcCommission(n, p);
+			while (m - c - p * n < 0)
+			{
+				n--;
+				c = CalcCommission(n, p);
+			}
+			return n;
 		}
 		public void PrintState()
 		{
@@ -110,6 +127,19 @@ namespace Diploma
 			//if (is_last && pos == 0) { return; }
 			Trade (decision, d.v, is_last);
 		}
+		// mode == 0 - asset
+		// mode == 1 - forts
+		private double CalcCommission(int amount, double price)
+		{
+			if (type == 0)		// asset
+			{
+				return amount * price * commission;
+			}
+			else
+			{
+				return amount * commission;
+			}
+		}
 		public void PrintHistory()
 		{
 			foreach (ArrayList x in his)
@@ -122,6 +152,16 @@ namespace Diploma
 			get { return pos == 0 ? 0 : l_price; }
 			set { l_price = value; }
 		}
+		private List<double> strat_params;
+		public List<double> pars
+		{
+			get { return strat_params; }
+			set { strat_params = value; }
+		}
+		public double commission
+		{
+			get { return commission_rate; }
+		}
 		private double l_price;
 		private int position;
 		private double mon;
@@ -130,6 +170,14 @@ namespace Diploma
 		private List<ArrayList> his;
 		private int count;
 		private int last_data;
+		private double commission_rate;
+		// 0 - asset
+		// 1 - futures
+		private int asset_type;
+		public int type
+		{
+			get { return asset_type; }
+		}
 		public int pos
 		{
 			get { return position; }
@@ -153,6 +201,10 @@ namespace Diploma
 				//Console.WriteLine(x[1]);
 				return x;
 			}
+		}
+		public double AY
+		{
+			get { return ((double)state[1] - (double)his[0][1]) / (double)his[0][1]; }
 		}
 		private int TestStrat(List<Data> d)
 		{
@@ -181,6 +233,33 @@ namespace Diploma
 			else
 			{
 				return (int)(m / price);
+			}
+		}
+		private int Strat1(List<Data> d)
+		{
+			if (d.Count == 1) return 0;
+			double l = last_price;
+			double a = pars[0];		// param
+			int p = pos;
+			//double m = money;
+			int last_index = d.Count - 1;
+			double price = d[last_index].v;
+			if (p == 0)		// если мгновенное отклонение большое, открыть позицию
+			{
+				double ds = (d[last_index].v - d[last_index - 1].v) / d[last_index - 1].v;
+				if (Math.Abs (ds) >= a)		// относительное приращение больше заданного
+				{
+					return (ds < 0.0 ? 1 : -1) * (int)(money / price);
+				}
+				return 0;
+			}
+			else 			// закрыть позицию
+			{
+				//return -p;
+				double c = Math.Abs(CalcCommission(p, price));
+				if ((price - l) * p - c > 0) { return -p; }
+				else if (((price - l) * p - c) / (l * p) <= -0.01) { return -p; }
+				else { return 0; }
 			}
 		}
 	}
@@ -224,23 +303,40 @@ namespace Diploma
 		{
 			List<Data> all_data = ReadData ("SBER.txt");
 			List<Data> d = FactorSeconds(all_data);
-			for (int i = 0; i < 8; ++i)
+			/*for (int i = 0; i < d.Count; ++i)
 			{
 				//Console.WriteLine (all_data[i].dt + " " + all_data[i].v + " " + all_data[i].volume);
 				Console.WriteLine(d[i].dt + " " + d[i].v + " " + d[i].volume);
-			}
-			
-			/*Robot r = new Robot(1000, null, 8-1);
-			//Console.WriteLine(r.last_price);
-			//r.Move(all_data[0]);
-			for (int i = 0; i < 8; ++i)
+			}*/
+			int count = d.Count;
+			for (int i = 0; i < count; ++i)
 			{
-				r.Move(all_data[i]);
+				Console.WriteLine(d[i].ToString());
 			}
-			Console.WriteLine("History:");
-			r.PrintHistory();
-			Console.WriteLine("State:");
-			r.PrintState();*/
+			double comm = 0.0001;
+			//double comm = 0.0;
+			double step = 0;
+			double h = 0.0001;
+			List<string> lines = new List<string>();
+			while (step < 0.01)
+			{
+				Robot r = new Robot(1000, null, count - 1, new List<double>(new double[] { step }), comm, 0);
+				for (int i = 0; i < count; ++i)
+				{
+					r.Move (d[i]);
+				}
+				//Console.WriteLine("History:");
+				//r.PrintHistory();
+				//Console.WriteLine("AY = " + r.AY);
+				lines.Add(step.ToString() + " " + r.AY);
+				step += h;
+			}
+			StreamWriter wr = new StreamWriter("Strat1/SBER.txt");
+			foreach(string s in lines)
+			{
+				wr.WriteLine(s);
+			}
+			wr.Close();
 		}
 	}
 }
